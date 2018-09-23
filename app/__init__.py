24493d626ca6cfe563f727b490c347d931091862
kikimay <marynmwirigi@gmail.com>
2018-09-23 04:54:07 -0700

    [chore #160703776] modified travis

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
 
+# Initialize the app
+app = Flask(__name__, instance_relative_config=True)
+
 # Load the views
+
 
 from app import views
 
 
 
 
-# Initialize the app
-app = Flask(__name__, instance_relative_config=True)
 
 
 
